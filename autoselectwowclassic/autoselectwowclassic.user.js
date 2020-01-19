14ec0f980e73c3bccdf953569b74d083bd5878e6
typoguy <typoguy@users.noreply.github.com>
2020-01-18 21:47:37 -0600

    Add autoselectwowclassic script

---
+++ autoselectwowclassic/autoselectwowclassic.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Auto-select WoW Classic
+// @description  Auto-select the "WoW Classic" filter option on Curseforge's WoW addon section
+// @version      0.1
+// @author       Phase
+// @match        https://www.curseforge.com/wow/addons
+// @grant        none
+// ==/UserScript==
+
+$("#filter-game-version option:contains(WoW Classic)").attr('selected', 'selected');
